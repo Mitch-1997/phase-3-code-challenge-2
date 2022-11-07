abfe794bd938ff86744cef8a64b5530bc6dfc1c5
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2022-11-07 17:37:52 +0300

    Completed assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-    
+    has_many :reviews 
+    has_many :products, through: :reviews 
+    def favorite_product 
+        self.reviews.order(:star_rating).last
+   end
+   def remove_reviews(product) 
+    self.reviews.where(product_id: product.id).destroy_all
+   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@
 puts "Deleting old data..."
 Product.destroy_all
 User.destroy_all
+Review.destroy_all
 
 puts "Creating users..."
 user1 = User.create(name: Faker::Name.name)
@@ -22,5 +23,11 @@ puts "Creating reviews..."
 # * and a review belongs to an user.                              *
 # *****************************************************************
 # Create Reviews Here
-
+Review.create(comment:"Good", star_rating: 5, user_id: user1.id, product_id: product1.id)
+Review.create(comment:"Bad", star_rating: 2, user_id: user2.id, product_id: product3.id)
+Review.create(comment:"Great", star_rating: 4, user_id: user3.id, product_id: product2.id)
+Review.create(comment:"Best", star_rating: 5, user_id: user2.id, product_id: product2.id)
+Review.create(comment:"Worst", star_rating: 1, user_id: user3.id, product_id: product1.id)
+Review.create(comment:"Good", star_rating: 4, user_id: user1.id, product_id: product4.id)
+Review.create(comment:"Best", star_rating: 5, user_id: user2.id, product_id: product5.id)
 puts "Seeding done!"
